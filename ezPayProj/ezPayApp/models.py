139da898a0da4edacfddf18ee849e2e7ded53017
RITWIK-PC\Ritwik <ritwik.rajendra8119@yahoo.in>
2018-03-23 23:30:36 -0700

    Added views and models for login and home

--- ezPayProj/ezPayApp/models.py
+++ ezPayProj/ezPayApp/models.py
@@ -4,3 +4,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=500)
+    auth = models.CharField(max_length=200)
+
+	def __unicode__(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class PayUser(models.Model):
+    stuId = models.CharField(max_length=100, primary_key = True)
+    name = models.CharField(max_length=200)
+    balance = models.FloatField(default = 0.0)	
+    dueDate = models.DateTimeField()
+    has_acc = = models.BooleanField(default=False)
+
+	def __unicode__(self):
+        return self.name
+    def __str__(self):
+        return self.name
--- ezPayProj/ezPayApp/views.py
+++ ezPayProj/ezPayApp/views.py
@@ -2,5 +2,42 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from ezPayApp.models import *
 
 # Create your views here.
+
+def loginpage(request):
+	if request.user and request.user.is_authenticated:
+		return HttpResponseRedirect(reverse('home'))
+	context={}
+	errors=[]
+
+	if request.method == 'GET':
+		schools = School.objects.all()
+		context['schools'] = schools
+   		return render(request,'login.html', context)
+
+   	schId = request.POST.get('sch_id')
+   	sch = School.objects.get(id = schId)
+
+   	return HttpResponseRedirect(reverse('schlogin/' + sch.id))
+
+
+def schlogin(request, schId):
+	
+	context = {}
+	if request.method == 'GET':
+		return render(request, 'sch_login.html', context)
+	else:
+		return HttpResponseRedirect(reverse('home'))
+
+def home(request):
+	user = PayUser.objects.get(id = request.user)
+	context['user'] = user
+	return render(request, 'home.html', context)
+
+	
+
+
+
+
